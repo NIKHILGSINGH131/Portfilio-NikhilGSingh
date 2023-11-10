dfe53c33d830d4b8c73f755d07ba796247b8885c
Nikhil G Singh <nikhilgsingh007@gmail.com>
2023-11-10 22:16:29 +0530

    fullstack web developer

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container hearder__container"> 
         <h5>Hello I'm</h5>
         <h1>Nikhil G Singh</h1>
-        <h5 className="text-light">Frontend Developer</h5>
+        <h5 className="text-light">Full-Stack Web Developer</h5>
         <CTA/>
         <HeaderSocilas/>
 
